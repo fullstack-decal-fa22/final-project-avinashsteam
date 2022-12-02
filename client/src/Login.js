b29211b4cc6448acc50bd215635a740c2c3f70ca
Yamuna Rao <yamunarao104@gmail.com>
2022-12-01 17:29:03 -0800

    authentication

--- client/src/Login.js
+++ client/src/Login.js
@@ -3,7 +3,7 @@ import "./App.css";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Input, Stack, Box, Button, ButtonGroup } from '@chakra-ui/react'
+import { Input, Stack, Box, Button, ButtonGroup, Text} from '@chakra-ui/react'
 import Header from "./Header";
 
 
@@ -15,6 +15,7 @@ import Header from "./Header";
 function Login(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [hasSubmitted, setHasSubmitted] = useState(false);
 
     const handleUsername = (event) => {
     console.log(event.target.value);
@@ -28,12 +29,26 @@ function Login(){
 
     const onSubmit = () => {
     axios.post('http://localhost:3000/user/login', {username, password}).then(res => console.log(res));
+    setHasSubmitted(true);
     // add in postman json object to check if it's working
     }
+
+    const colorChange = (value) => {
+        let color
+        if (value){
+            color = "green"
+        } else {
+            color = "#9BCBE3"
+        }
+        return color
+      }
     
     return (
         <div style={{backgroundColor:'#C8EDF7'}}>
+           
+
             <Header></Header>
+           
             <Box  bg='#C8EDF7' w="100%" display="flex" justifyContent="center" alignItems="center" paddingTop='10%' paddingBottom='100%'>
             <Box  bg='#9BCBE3' display="flex" w='500px' h='350px' justifyContent="center" alignItems="center" >
             <Stack spacing={3} w="25%" align = 'center'>
@@ -41,7 +56,8 @@ function Login(){
             <Input placeholder='Login' size='md' w='300px' bg='white' />
             <Input placeholder='Password' size='md' w='300px' bg='white' />
             <Button w="50%" color='#FFFFFF' bg="#216583" >Login</Button> */}
-            <b >Login - Welcome back!</b>
+            <Text as='b'> Login </Text>
+            <Text color={colorChange(hasSubmitted)}> Welcolme {username} </Text>
             <Input placeholder='Login' size='md' w='300px' bg='white' onChange={handleUsername} />
             <Input placeholder='Password' size='md' w='300px' bg='white' onChange={handlePassword} />
             <Button w="50%" color='#FFFFFF' bg="#216583" onClick={onSubmit}>Login</Button>
